8a3958b2be0c8373b683d7e431d1043c1ce1c886
MathleteDev <mathletedev@gmail.com>
2021-02-19 21:27:15 -0800

    React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
-export default () => {
-	return <div>Hello World!</div>;
+import { BrowserRouter, Route } from "react-router-dom";
+import { Home } from "./Home";
+
+export default (): JSX.Element => {
+	return (
+		<BrowserRouter>
+			<Route path="/" component={Home} />
+		</BrowserRouter>
+	);
 };
---
+++ src/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+interface Props {}
+
+export const Home: React.FC<Props> = (): JSX.Element => {
+	return <div>Home</div>;
+};
